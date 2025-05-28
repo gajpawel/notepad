2518b55f0d86ca370a48f4ff7c1cc3c5f2035c34
gajpawel <pg305038@student.polsl.pl>
2025-05-28 21:18:59 +0200

    Poprawki do firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,11 +6,23 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+}
 android {
     namespace = "com.example.notepad"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 import 'screens/login/auth_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+const FirebaseOptions firebaseOptions = FirebaseOptions(
+  apiKey: "AIzaSyA7TvPSmjOk3qZirF_u3dXVCt7a0rHTyDs",
+  authDomain: "notepaddb-5e65b.firebaseapp.com",
+  databaseURL: "https://notepaddb-5e65b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "notepaddb-5e65b",
+  storageBucket: "notepaddb-5e65b.firebasestorage.app",
+  messagingSenderId: "688158558009",
+  appId: "1:688158558009:web:c5260066cfbe7469a30ae8",
+  measurementId: "G-52CD72846N",
+);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: firebaseOptions,
+    );
+  } catch (e) {
+    print('Błąd inicjalizacji Firebase: $e');
+  }
   runApp(const MyApp());
 }
 
